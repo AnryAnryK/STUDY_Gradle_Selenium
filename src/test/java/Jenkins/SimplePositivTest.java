fbdc879ae43767405c144f76bb664d1d2ca136d1
AnryAnryK <andkay@yandex.ru>
2024-08-21 11:30:31 +0300

    2024.08.21 - Урок 17 - Jenkins / Simple тесты - создал ветку "Jenkins"

--- src/test/java/Jenkins/SimplePositivTest.java
+++ src/test/java/Jenkins/SimplePositivTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+//тесты в Jenkins
 
 @Tag("Simple")
 public class SimplePositivTest {
